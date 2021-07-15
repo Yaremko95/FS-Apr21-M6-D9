28be9b3c127eab64c7196790551bb00e5496f9a4
tetiana <tetianayaremko@gmail.com>
2021-07-15 12:02:18 +0300

    adds relationships

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,19 @@
 import Product from "./models/product.js";
 import User from "./models/user.js";
 import Category from "./models/category.js";
+import userProduct from "./models/userProduct.js";
 import sequlize from "./models/index.js";
 
 Category.hasMany(Product);
 Product.belongsTo(Category);
 
-export { Product, sequlize, User, Category };
+Product.belongsToMany(User, { through: { model: userProduct, unique: false } });
+User.belongsToMany(Product, { through: { model: userProduct, unique: false } });
+
+User.hasMany(userProduct);
+userProduct.belongsTo(User);
+
+Product.hasMany(userProduct);
+userProduct.belongsTo(Product);
+
+export { Product, sequlize, User, Category, userProduct };
